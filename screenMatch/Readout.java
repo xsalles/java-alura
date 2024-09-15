4caf4460e89da576b7d8a6be28e48f21e918b9db
xsalles <xs.salles@gmail.com>
2024-09-15 10:14:44 -0300

    feat: created the input for the user type the year of realease of the movie.

--- screenMatch/Readout.java
+++ screenMatch/Readout.java
@@ -5,11 +5,14 @@ public class Readout {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String favoriteMovie;
+        int yearOfRealease;
 
         System.out.println("Digite seu filme favorito");
         favoriteMovie = sc.nextLine();
-        System.out.println(favoriteMovie);
-
+       
+        System.out.println("Qual foi o ano de lançamento desse filme?");
+        yearOfRealease = sc.nextInt();
+        
 
     }
 }
