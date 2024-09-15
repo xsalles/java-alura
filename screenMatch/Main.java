38494532d86039ea01c285b87fbb66177a4015f5
xsalles <xs.salles@gmail.com>
2024-09-14 23:20:15 -0300

    feat: created a start of the screen match and added a movie to the list, and showed the year of realease and the rate of the movie.

---
+++ screenMatch/Main.java
@@ -0,0 +1,15 @@
+package screenMatch;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Seja bem vindo ao Screen Match \n");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean inclusoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        System.out.println("A nota do filme é: " + notaDoFilme);
+    }
+}
