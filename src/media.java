49124fbaf13605384e77eac398721f790ab3a869
xsalles <xs.salles@gmail.com>
2024-09-15 09:19:18 -0300

    feat: created a average between two numbers and showed in the output.

---
+++ src/media.java
@@ -0,0 +1,12 @@
+public class media {
+    public static void main(String[] args) {
+        double num1, num2, media;
+
+        num1 = 8.3;
+        num2 = 3.2321312;
+        
+        media = (num1 + num2) /2;
+
+        System.out.printf("A média desses dois números é: %.1f", media);
+    }
+}
