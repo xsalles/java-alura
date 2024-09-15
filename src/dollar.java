803265d84c1f6949194904f5a4a8f69c0770e45f
xsalles <xs.salles@gmail.com>
2024-09-15 09:39:55 -0300

    feat: created a concatenation with the value of the dollar and converted him to real.

---
+++ src/dollar.java
@@ -0,0 +1,10 @@
+public class dollar {
+    public static void main(String[] args) {
+        double dollar = 25.0;
+        double valueOfTheDollarInReal = 4.94;
+
+        double converting = dollar * valueOfTheDollarInReal;
+
+        System.out.printf("Se você tem U$%.2f em reais você teria R$%.2f reais", dollar, converting );
+    }
+}
