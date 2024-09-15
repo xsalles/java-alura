71a0c6e49492db08db3a802ae0c8822bead26a40
xsalles <xs.salles@gmail.com>
2024-09-15 10:21:46 -0300

    feat: created a sysout for the all informations that user has typed.

--- screenMatch/Readout.java
+++ screenMatch/Readout.java
@@ -6,6 +6,7 @@ public class Readout {
         Scanner sc = new Scanner(System.in);
         String favoriteMovie;
         int yearOfRealease;
+        double rate;
 
         System.out.println("Digite seu filme favorito");
         favoriteMovie = sc.nextLine();
@@ -13,6 +14,12 @@ public class Readout {
         System.out.println("Qual foi o ano de lançamento desse filme?");
         yearOfRealease = sc.nextInt();
         
+        System.out.println("Qual a avaliação para esse filme?");
+        rate = sc.nextDouble();
+
+        System.out.printf("O seu filme favorito é %s \n", favoriteMovie);
+        System.out.printf("O ano de lançamento é %d \n", yearOfRealease);
+        System.out.printf("O a nota para esse filme é %.2f \n", rate);
 
         sc.close();
 
