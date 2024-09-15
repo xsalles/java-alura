78b0ebf5c777ddac2a6dea8bb2612f368b8bbbfa
xsalles <xs.salles@gmail.com>
2024-09-15 09:26:35 -0300

    feat: concatenation a phrase with the correct option.

---
+++ src/concatenation.java
@@ -0,0 +1,8 @@
+public class concatenation {
+    public static void main(String[] args) {
+        char letter = 'A';
+        String phrase = "A opção correta é: ";
+
+        System.out.printf(phrase + letter);
+    }
+}
