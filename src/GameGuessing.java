4f2e02f61430bacd0122a16d36a75bef9ec5ee35
xsalles <xs.salles@gmail.com>
2024-09-15 13:23:45 -0300

    feat: created a number random and if the user match the number, it's showed a congratulations message, and if the user get the number wrong, he\she has a number of attempts limit.

---
+++ src/GameGuessing.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GameGuessing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberTyped;
+        int numberRandom = new Random().nextInt(100);
+        int numberOfAttempts = 0;
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println("Digite o valor do número secreto!");
+            numberTyped = sc.nextInt();
+
+            numberOfAttempts++;
+            
+            if (numberTyped == numberRandom) {
+                System.out.println("PARABÉNS, você acertou dentro de " + numberOfAttempts + " tentativas! O número era " + numberRandom);
+                break;
+            } else if (numberTyped > numberRandom) {
+                System.out.println("O valor que você digitou é maior que o número aleatório");
+                
+            }else {
+                System.out.println("O valor que você digitou é menor que o número aleatório");
+            } 
+
+            if (numberOfAttempts == 20){
+                System.out.println("Infelizmente você atingiu o limite de tentativas. :(");
+            }
+
+        }
+        sc.close();
+    }
+}
