95bab30df96c1e467ec0e1e3ee7ec7e8591f45e6
xsalles <xs.salles@gmail.com>
2024-09-15 09:22:34 -0300

    feat: casted a number.

---
+++ src/casting.java
@@ -0,0 +1,8 @@
+public class casting {
+    public static void main(String[] args) {
+        double number = 2.876;
+        int numberCasted = (int) number;
+
+        System.out.println(numberCasted);
+    }
+}
