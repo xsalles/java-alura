c4d59a60da3585e284c36ba62ae40ba474cfb93c
xsalles <xs.salles@gmail.com>
2024-09-15 13:36:30 -0300

    feat: created a input for the user type a number, then it's showed if the number is positive or negative, and if the number is 0, it's showed that number is neutral.

---
+++ src/PositiveOrNegative.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PositiveOrNegative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberTyped;
+
+        System.out.println("Digite o primeiro número inteiro que vier na sua cabeça");
+        numberTyped = sc.nextInt();
+
+        if(numberTyped == 0){
+            System.out.println("O número '0' é um número neutro");
+        } else if (numberTyped > 0) {
+            System.out.println("O número que você digitou é positivo");
+        } else {
+            System.out.println("O número que você digitou é negativo");
+        }
+
+        sc.close();
+
+    }
+}
